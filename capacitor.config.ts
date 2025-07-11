bd3bc79b3e7a341ea8153aac65a0d938ba9fe5b2
casper-justus <138237661+casper-justus@users.noreply.github.com>
2025-07-11 15:03:06 +0000

    appification

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kazpian.kazpusic',
+  appName: 'kazpusic',
+  webDir: 'dist'
+};
+
+export default config;
